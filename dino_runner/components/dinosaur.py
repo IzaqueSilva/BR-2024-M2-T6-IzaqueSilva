3b4be632f13be99a9cee90bbe61b2c5f485903ca
IzaqueSilva <Izaque.Brito@fundacion-jala.org>
2023-09-13 22:26:45 -0300

    Aula3: Obstacles

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Sprite
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
@@ -6,7 +7,7 @@ Y_POS = 310
 JUMP_VEL = 8.5
 Y_POS_DUCK = 340
 
-class Dinosaur:
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -16,6 +17,7 @@ class Dinosaur:
         self.jump_vel = JUMP_VEL
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
 
 
     def update(self, user_input):
@@ -26,17 +28,17 @@ class Dinosaur:
         elif self.dino_duck:
             self.duck()
             
-        if user_input[pygame.K_UP] and not self.dino_jump:
+        if (user_input[pygame.K_UP] or user_input [pygame.K_SPACE] or user_input[pygame.K_w]) and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
             self.dino_duck = False
 
-        if user_input[pygame.K_DOWN] and not self.dino_jump:
+        elif (user_input[pygame.K_DOWN]or user_input [pygame.K_s]) and not self.dino_jump:
             self.dino_duck = True
             self.dino_jump = False
             self.dino_run = False
 
-        elif not self.dino_jump or user_input[pygame.K_DOWN]:
+        elif not self.dino_jump and not self.dino_duck:
             self.dino_run = True
             self.dino_jump = False
             self.dino_duck = False
@@ -70,6 +72,7 @@ class Dinosaur:
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS_DUCK
         self.step_index += 1
+        self.dino_duck = False
 
     def draw(self, screen):
         screen.blit(self.image,(self.dino_rect.x, self.dino_rect.y))

451289e25202417ae0e160bb7cb00af9f997a447
IzaqueSilva <Izaque.Brito@fundacion-jala.org>
2023-09-18 19:19:57 -0300

    criaçao da classe cloud e modificaçoes no bird

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -3,6 +3,7 @@ import pygame
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+from dino_runner.components.cloud import Cloud
 
 FONT_STYLE = "freesansbold.ttf"
 
@@ -15,13 +16,14 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = False
         self.playing = False
-        self.game_speed = 20
+        self.game_speed = 15
         self.score = 0 
         self.death_count = 0
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         self.player = Dinosaur()
         self.obstacle_manager = ObstacleManager()
+        self.cloud = Cloud()
 
     def execute(self):
         self.running = True
@@ -52,6 +54,7 @@ class Game:
         self.player.update(user_input)
         self.obstacle_manager.update(self)
         self.update_score()
+        self.cloud.update()
 
     def update_score(self):
         self.score += 1
@@ -65,6 +68,7 @@ class Game:
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
         self.draw_score()
+        self.cloud.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
